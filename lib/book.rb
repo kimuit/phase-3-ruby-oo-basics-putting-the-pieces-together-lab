2e82e55ce08911b8e123074d7398ea6a4b62abc5
kimuit117 <kemoikiuita@gmail.com>
2022-10-26 11:37:11 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,28 @@
+# class Book
+    
+#     attr_accessor :title,:author, :page_count, :genre
+   
+
+#     def initialize(title)
+#         @title = title
+#     end
+
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end
+    
+# end
+
 class Book
-    
-    attr_accessor :author, :page_count, :genre
-    
-    attr_reader :title
 
-    def initialize(title)
-        @title = title
-    end
+attr_accessor :title, :author, :page_count, :genre
 
-    def turn_page
-        puts "Flipping the page...wow, you read fast!"
-    end
-    
+def initialize(title)
+@title=title
+end
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,26 @@
 # Make your shoe class here!
 
+# class Shoe
+#   attr_accessor :brand, :color, :size, :material, :condition
+
+#   def initialize(brand)
+#     @brand = brand
+#   end
+
+# def cobble
+
+#   @condition = "new"
+#   puts "Your shoe is as good as new!"
+
+# end
+# end
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-
-  def initialize(brand)
-    @brand = brand
-  end
-
-def cobble
-
-  @condition = "new"
-  puts "Your shoe is as good as new!"
-
+ def initialize(brand)
+@brand=brand
+ end
+def cobble (condition="new")
+  @condition=condition
+puts "Your shoe is as good as new!"
 end
 end
